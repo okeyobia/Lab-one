0746b4b007bad3705d2bba44608c76175ff5cc85
Okey Obiajunwa <okey.obia@gmail.com>
2020-06-12 00:17:36 -0400

    add coinToss with functions and driver client

--- src/main/java/com/okeyobia/questiontwo/CoinToss.java
+++ src/main/java/com/okeyobia/questiontwo/CoinToss.java
@@ -1,4 +1,20 @@
 package com.okeyobia.questiontwo;
 
+import java.util.Random;
+
 public class CoinToss {
+
+    public enum Coin {
+        heads, tails
+    };
+
+    public Coin flipCoin() {
+        Random random = new Random();
+        int value = random.nextInt(2);
+        return value == 1 ? Coin.heads : Coin.tails;
+    }
+
+    public String getValue(String str){
+        return (str.equalsIgnoreCase("heads")) ? "tails" : "heads";
+    }
 }
--- src/main/java/com/okeyobia/questiontwo/DeliverableTwo.java
+++ src/main/java/com/okeyobia/questiontwo/DeliverableTwo.java
@@ -6,38 +6,29 @@ import java.util.Scanner;
 public class DeliverableTwo {
 
     public static void main(String[] args) {
-        int correctCount =0;
         Scanner readValue = new Scanner(System.in);
+        int correctCount =0;
         System.out.println("Guess which will have more: heads or tails?");
         String headsOrTailGuess = readValue.next();
         System.out.println("How many times shall we flip a coin?");
         int numberOfFlips = readValue.nextInt();
-        DeliverableTwo dt = new DeliverableTwo();
-
+        CoinToss toss = new CoinToss();
 
         for (int i = 1; i <= numberOfFlips; i++) {
-            if (dt.flipCoin().toString().equalsIgnoreCase(headsOrTailGuess)) {
+            if (toss.flipCoin().toString().equalsIgnoreCase(headsOrTailGuess)) {
                 System.out.println(headsOrTailGuess);
                 correctCount++;
             }else
-                System.out.println(getValue(headsOrTailGuess));
+                System.out.println(toss.getValue(headsOrTailGuess));
         }
-        double result = (double) (correctCount * 100) / numberOfFlips;
-        System.out.println(result + "%");
+        double result = calculateStatistics(numberOfFlips, correctCount);
+        String stringResult =String.format("%.2f", result);
+        System.out.println("Your guess, " +  headsOrTailGuess +", came up "+ correctCount + " time(s).\n"   +  "That's "+ stringResult + "%");
+        readValue.close();
     }
 
-    private static String getValue(String str){
-        return (str.equalsIgnoreCase("heads")) ? "Tails" : "Heads";
-    }
-
-    private enum Coin {
-        Heads, Tails
-    };
-
-    private Coin flipCoin() {
-        Random random = new Random();
-        int value = random.nextInt(2);
-        return value == 1 ? Coin.Heads : Coin.Tails;
+    private static double calculateStatistics(int numberOfFlips, int correctCount) {
+         return (double) (correctCount * 100) / numberOfFlips;
     }
 
 }
